f0bfc096a2202178d3a31a4dfe6318f4df8488b8
cramos <chloieeeramos@gmail.com>
2019-08-16 19:34:13 -0400

    updated files

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendsList = [
+	{
+	  name: "Brad Pitt",
+	  photo: "https://en.wikipedia.org/wiki/Brad_Pitt#/media/File:Brad_Pitt_2019_by_Glenn_Francis.jpg",
+	  preferences: [1, 2, 4, 3, 4, 5, 2, 1, 5, 3]
+	},
+
+	{
+	  name: "Leonardo DiCaprio",
+	  photo: "https://www.google.ca/url?sa=i&source=images&cd=&ved=2ahUKEwiulYeAxojkAhUwc98KHXuTBVoQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.harpersbazaar.com%2Fbeauty%2Fhair%2Fg6735%2Fleonardo-dicaprio-evolution%2F&psig=AOvVaw33hM-g_x5GKhpNJUybSklj&ust=1566084622891324",
+	  preferences: [1, 2, 5, 3, 4, 5, 1, 3, 5, 2]
+	},
+
+	{
+	  name: "Beyonce",
+	  photo: "https://www.google.ca/url?sa=i&source=images&cd=&ved=2ahUKEwigoYLSxojkAhWRUt8KHXbkAUEQjRx6BAgBEAQ&url=https%3A%2F%2Fus.hola.com%2Fhealth-and-beauty%2F2019062725268%2Fbeyonce-tina-lawson-natural-hair%2F&psig=AOvVaw3O0q0WoHG6oQEN_rNyvT4e&ust=1566084755668140",
+	  preferences: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+	},
+];
+
+module.exports = friendsList;
